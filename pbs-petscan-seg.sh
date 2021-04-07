6cae072ace132e5367c115b48c8ef97d007c17bc
KinanAlAttar <50109313+KinanAlAttar@users.noreply.github.com>
2021-04-06 21:20:59 -0400

    Add files via upload

---
+++ pbs-petscan-seg.sh
@@ -0,0 +1,16 @@
+#PBS -N pet-seg
+#PBS -l walltime=1:30:00
+#PBS -l nodes=1:ppn=20:gpus=1
+#PBS -j oe
+#PBS -A PAS1759
+
+# uncomment if using qsub
+cd $PBS_O_WORKDIR
+echo $PBS_O_WORKDIR
+
+module reset
+source $HOME/osc_classes/PHYSICS6820/mypython/bin/activate
+which python
+module load cuda/10.1.168
+
+time python3 kaggle-pet-data.py
